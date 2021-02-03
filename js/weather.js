d21edb41dc447d2f0339c2740dc8a9278277ef0e
Miyazono1 <44627339+Miyazono1@users.noreply.github.com>
2021-02-02 21:32:48 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ const descElement = document.querySelector('.temperature-description p');
 // App data
 const weather = {};
 weather.temperature = {
-  unit: 'celsius',
+  unit: 'fahrenheit',
 };
 
 // Change to 'F' for Fahrenheit
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 40.770081;
+  let longitude = -73.103912;
 
   getWeather(latitude, longitude);
 }
